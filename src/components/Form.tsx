0ed54d085e1d2f2b122b52398465e788af8b03be
Kai Takami <kaitakami@protonmail.com>
2023-01-09 20:05:17 +0900

    Implement handleSubmit and handleChange functions

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,45 @@
-const Form = () => {
-    const handleSubmit = () => {
+import { Sub } from "../types";
+import useNewSubForm from "../hooks/useNewSubForm";
+interface FormProps {
+    onNewSub: (newSub: Sub) => void
+}
 
+const Form = ({ onNewSub }: FormProps) => {
+
+    const [inputValues, dispatch] = useNewSubForm()
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        onNewSub(inputValues)
+        handleClear()
+    }
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+        dispatch({
+            type: "change_value",
+            payload: {
+                inputName: e.target.name,
+                inputValue: e.target.value
+            }
+        })
+    }
+
+    const handleClear = () => {
+        dispatch({
+            type: "clear"
+        })
     }
 
     return (
         <div>
             <h3>Add new user</h3>
             <form onSubmit={handleSubmit}>
-                <input type="text" name="nick" placeholder="nick" />
-                <input type="text" name="subMonths" placeholder="nick" />
-                <input type="text" name="avatar" placeholder="nick" />
-                <input type="text" name="description" placeholder="nick" />
-                <button>Save new sub!</button>
+                <input onChange={handleChange} value={inputValues.nick} type="text" name="nick" placeholder="nick" />
+                <input onChange={handleChange} value={inputValues.subMonths} type="text" name="subMonths" placeholder="subMonths" />
+                <input onChange={handleChange} value={inputValues.avatar} type="text" name="avatar" placeholder="avatar" />
+                <textarea onChange={handleChange} value={inputValues.description} name="description" placeholder="description" />
+                <button onClick={handleClear} type="button">Clear the form</button>
+                <button type="submit">Save new sub!</button>
             </form>
         </div>
     );
